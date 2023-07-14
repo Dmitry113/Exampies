0231bfe36f0dece8af5aae18c1538264494e5297
Dmitry Krupin <krdv13@yandex.ru>
2023-07-15 01:01:57 +0700

    Программа выводит дни недели по заданному числу

---
+++ C#/Seminar/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №3. Напишите программу, которая будет выдовать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Введите число: ");
+
+int number1 = int.Parse(Console.ReadLine());
+if (number1 == 1) Console.WriteLine("Сегодня Понедельник");
+if (number1 == 2) Console.WriteLine("Сегодня Вторник");
+if (number1 == 3) Console.WriteLine("Сегодня Среда");
+if (number1 == 4) Console.WriteLine("Сегодня Четверг");
+if (number1 == 5) Console.WriteLine("Сегодня Пятница");
+if (number1 == 6) Console.WriteLine("Сегодня Суббота");
+if (number1 == 7) Console.WriteLine("Сегодня Воскресенье");
+
