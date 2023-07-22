1ff9244412699565d7e8249c3242711b005b6994
Dmitry Krupin <krdv13@yandex.ru>
2023-07-22 19:25:15 +0700

    .Программа которая удаляет вторую цифру из числа

---
+++ C#/Seminar/Seminar 2/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача №11. Напишите программу, которая выводит случайное трёхзначное число и
+// удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int rondom = new Random().Next(100, 1000);
+int firstDigit = rondom / 100;
+int thirdDigit = rondom % 10;
+
+Console.WriteLine($"{rondom} -> {firstDigit }{thirdDigit}");
