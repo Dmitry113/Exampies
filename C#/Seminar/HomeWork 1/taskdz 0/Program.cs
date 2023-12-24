f7e823f19c628fe30413e08f6bdc040b21a2ab88
Dmitry Krupin <krdv13@yandex.ru>
2023-12-24 17:21:25 +0700

    Программа которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ C#/Seminar/HomeWork 1/taskdz 0/Program.cs
@@ -0,0 +1,28 @@
+﻿//Программа которая принимает на вход трёхзначное число и на выходе показывает 
+//вторую цифру этого числа
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите трёхзначное число:");
+        string input = Console.ReadLine();
+
+        if (input.Length == 3 && int.TryParse(input, out int number))
+        {
+            int secondDigit = (number / 10) % 10;
+            Console.WriteLine($"Вторая цифра числа {input}: {secondDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите трёхзначное число.");
+        }
+    }
+}
---
+++ C#/Seminar/HomeWork 1/taskdz1/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        string input = Console.ReadLine();
+
+        char thirdDigit = GetThirdDigit(input);
+
+        if (thirdDigit != '\0')
+        {
+            Console.WriteLine($"Третья цифра: {thirdDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет.");
+        }
+    }
+
+    static char GetThirdDigit(string input)
+    {
+        // Проверка наличия третьей цифры в числе
+        if (input.Length >= 3 && Char.IsDigit(input[2]))
+        {
+            return input[2];
+        }
+        else
+        {
+            return '\0';
+        }
+    }
+}
